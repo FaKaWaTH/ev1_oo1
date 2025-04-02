39447bad3fdb3bece39067cccd233e43f05efb30
Diego Llorens <>
2025-04-01 21:38:42 -0300

    Implementacion de class Usuario

---
+++ src/main/java/Code/Usuario.java
@@ -0,0 +1,32 @@
+
+package Code;
+
+public class Usuario {
+    int id;
+    
+    String nombre;
+    String correo;
+    
+    boolean mayorEdad;
+
+    public Usuario() {}
+    
+    public void registrar(int id, String nombre) {
+        
+        this.nombre = nombre;
+    }
+    
+    public void registrar(int id, String nombre, String correo) {
+        
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+    
+    public void registrar(int id, String nombre, String correo, boolean mayorEdad) {
+        
+        this.nombre = nombre;
+        this.correo = correo;
+        this.mayorEdad = mayorEdad;
+    }
+    
+}
